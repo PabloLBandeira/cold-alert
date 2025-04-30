7bbab9bb67ab15979752ba6ec743ce04a90d5cd0
PabloBandeira <pablo.bandeira@ymail.com>
2025-04-30 19:30:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-cold-alert h-screen bg-cover bg-center">
+      {/* Você pode adicionar conteúdo aqui depois */}
+    </div>
+  )
+}
