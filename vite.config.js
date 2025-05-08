0d6f80843409648e7ed0c7c26ee80f51d59dcb7a
PabloBandeira <pablo.bandeira@ymail.com>
2025-05-07 23:44:16 -0300

    feat: insert path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "",
+  base: "/cold-alert/",
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs', 
