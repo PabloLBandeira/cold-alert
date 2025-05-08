30906e56b1540d2346e651c788af8b17ba5f553d
PabloBandeira <pablo.bandeira@ymail.com>
2025-05-07 23:52:06 -0300

    fix: images path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Container from './components/Container'
 
 export default function App() {
   return (
-    <div className="fixed inset-0 bg-cover bg-center overflow-y-auto" style={{ backgroundImage: 'url(/images/bg-cold-alert.jpg)'}}>
+    <div className="fixed inset-0 bg-cover bg-center overflow-y-auto" style={{ backgroundImage: 'url(/cold-alert/images/bg-cold-alert.jpg)'}}>
       <Container />
     </div>
   )
--- src/helpers/weatherImage.js
+++ src/helpers/weatherImage.js
@@ -6,17 +6,17 @@ function getWeatherImage (userTemp) {
   let alt;
 
   if (userTemp <= 0) {
-    image = "/images/capybara-frozen.svg", alt = "capivara congelada";
+    image = "/cold-alert/images/capybara-frozen.svg", alt = "capivara congelada";
   } else if (userTemp <= 10) {
-    image = "/images/capybara-cold.svg", alt = "capivara com frio";
+    image = "/cold-alert/images/capybara-cold.svg", alt = "capivara com frio";
   } else if (userTemp <= 15) {
-    image = "/images/capybara-relax.svg", alt = "capivara relax";
+    image = "/cold-alert/images/capybara-relax.svg", alt = "capivara relax";
   } else if (userTemp <= 20) {
-    image = "/images/capybara-surf.svg", alt = "capivara indo surfar em Matinhos";
+    image = "/cold-alert/images/capybara-surf.svg", alt = "capivara indo surfar em Matinhos";
   } else if (userTemp <= 27) {
-    image = "/images/capybara-heat.svg", alt = "capivara com muito calor";
+    image = "/cold-alert/images/capybara-heat.svg", alt = "capivara com muito calor";
   } else {
-    image = "/images/capybara-on-fire.svg", alt = "capivara pegadno fogo de tanto calor";
+    image = "/cold-alert/images/capybara-on-fire.svg", alt = "capivara pegadno fogo de tanto calor";
   }
 
   return {image, alt};
